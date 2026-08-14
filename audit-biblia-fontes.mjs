363f69d5ba5136876c08ab9523ec7ec2e5e4e006
Ansamar <124778721+Ansamar@users.noreply.github.com>
2026-08-14 10:01:01 +0200

    Audit multiple textual witnesses without inflating chapter coverage

--- audit-biblia-fontes.mjs
+++ audit-biblia-fontes.mjs
@@ -3,25 +3,33 @@ import {createClient} from '@sanity/client'
 const client = createClient({
   projectId: 'jc1k65lj',
   dataset: 'production',
-  apiVersion: '2026-08-13',
+  apiVersion: '2026-08-14',
   useCdn: false,
 })
 
 const query = `{
   "libri": count(*[_type == "libro"]),
   "capitoliAnalitici": count(*[_type == "capitolo"]),
-  "capitoliTesto": count(*[_type == "testoBiblicoCapitolo"]),
+  "documentiTesto": count(*[_type == "testoBiblicoCapitolo"]),
+  "capitoliConTesto": count(array::unique(*[_type == "testoBiblicoCapitolo" && defined(capitolo._ref)].capitolo._ref)),
   "versetti": count(*[_type == "testoBiblicoCapitolo"].versetti[]),
-  "testiSenzaLibro": *[_type == "testoBiblicoCapitolo" && !defined(libro->._id)]{_id, numero, "ref": libro._ref},
-  "testiSenzaCapitoloAnalitico": *[_type == "testoBiblicoCapitolo" && defined(capitolo) && !defined(capitolo->._id)]{_id, numero, "ref": capitolo._ref},
+  "testiSenzaLibro": *[_type == "testoBiblicoCapitolo" && !defined(libro->._id)]{_id, numero, tradizione, "ref": libro._ref},
+  "testiSenzaCapitoloAnalitico": *[_type == "testoBiblicoCapitolo" && defined(capitolo) && !defined(capitolo->._id)]{_id, numero, tradizione, "ref": capitolo._ref},
   "capitoliSenzaTesto": *[_type == "capitolo" && !(_id in *[_type == "testoBiblicoCapitolo"].capitolo._ref)]{_id, numero, titolo, "libro": libro->titolo},
   "libriConTesto": array::unique(*[_type == "testoBiblicoCapitolo"].libro._ref),
+  "tradizioniMultiple": *[_type == "libro" && count(*[_type == "testoBiblicoCapitolo" && libro._ref == ^._id].tradizione) > 1]{
+    _id,
+    titolo,
+    "tradizioni": array::unique(*[_type == "testoBiblicoCapitolo" && libro._ref == ^._id && defined(tradizione)].tradizione)
+  },
   "versettiVuotiAmmessi": *[_type == "testoBiblicoCapitolo" && count(versetti[testo == "" && statoTestuale in ["metatesto_solo", "omesso_nell_edizione"]]) > 0]{
     _id,
+    tradizione,
     "vuoti": versetti[testo == "" && statoTestuale in ["metatesto_solo", "omesso_nell_edizione"]]{numero, statoTestuale, notaEditoriale, metatesto}
   },
   "anomalieTestoVuoto": *[_type == "testoBiblicoCapitolo" && count(versetti[testo == "" && !(statoTestuale in ["metatesto_solo", "omesso_nell_edizione"])]) > 0]{
     _id,
+    tradizione,
     "anomalie": versetti[testo == "" && !(statoTestuale in ["metatesto_solo", "omesso_nell_edizione"]) ]{numero, statoTestuale, notaEditoriale, metatesto}
   }
 }`
@@ -31,11 +39,13 @@ try {
   const summary = {
     libri: result.libri,
     capitoliAnalitici: result.capitoliAnalitici,
-    capitoliTesto: result.capitoliTesto,
+    documentiTesto: result.documentiTesto,
+    capitoliConTesto: result.capitoliConTesto,
     versetti: result.versetti,
     testiSenzaLibro: result.testiSenzaLibro.length,
     testiSenzaCapitoloAnalitico: result.testiSenzaCapitoloAnalitico.length,
     capitoliSenzaTesto: result.capitoliSenzaTesto.length,
+    libriConTradizioniMultiple: result.tradizioniMultiple.length,
     documentiConVuotiAmmessi: result.versettiVuotiAmmessi.length,
     documentiConAnomalieTestoVuoto: result.anomalieTestoVuoto.length,
   }
@@ -45,6 +55,11 @@ try {
   console.log('====================================\n')
   console.log('RIEPILOGO')
   console.log(JSON.stringify(summary, null, 2))
+
+  if (result.tradizioniMultiple.length) {
+    console.log('\nLIBRI CON PIÙ TRADIZIONI / TESTIMONI')
+    console.log(JSON.stringify(result.tradizioniMultiple, null, 2))
+  }
 
   if (result.anomalieTestoVuoto.length) {
     console.log('\nANOMALIE: TESTO VUOTO NON GIUSTIFICATO')
